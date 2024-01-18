3b7083e7f2c758cc046bb73c40920ba4dbaee8dc
AyaMamat <148882937+AyaMamat@users.noreply.github.com>
2024-01-18 03:27:09 -0600

    Update src/main/java/com/laba/solvd/jdbc/service/interfaces/ISupplierService.java
    Co-authored-by: ankochergin <103670717+ankochergin@users.noreply.github.com>

--- src/main/java/com/laba/solvd/jdbc/service/interfaces/ISupplierService.java
+++ src/main/java/com/laba/solvd/jdbc/service/interfaces/ISupplierService.java
@@ -5,6 +5,8 @@ import com.laba.solvd.jdbc.model.Supplier;
 import java.util.List;
 
 public interface ISupplierService {
+
     Supplier create(Supplier supplier);
     List<Supplier> getAll();
+    
 }
